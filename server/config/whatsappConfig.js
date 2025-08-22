6f6fcd8f65874f0a3674a059cb9f56cefcdde24a
Nikhilcharan-dev <chintufru143@gmail.com>
2025-08-22 11:26:35 +0530

    Meta Testing

--- server/config/whatsappConfig.js
+++ server/config/whatsappConfig.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/webhook', async (req, res) => {
     try {
         console.log(req.query);
-        if(req.query['hub.mode'] === 'subscribe' && req.query['hub.token'] === process.env.WHATSAPP_VERIFY_TOKEN) {
+        if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.WHATSAPP_VERIFY_TOKEN) {
             return res.status(200).send(req.query['hub.challenge'])
         }
         return res.status(400);
