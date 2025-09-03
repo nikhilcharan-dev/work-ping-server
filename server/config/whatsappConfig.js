d362c15a2037091e9a043fb78494e46e9472cfdf
Nikhilcharan-dev <chintufru143@gmail.com>
2025-09-03 11:57:31 +0530

    Testing Webhook

--- server/config/whatsappConfig.js
+++ server/config/whatsappConfig.js
@@ -18,9 +18,51 @@ router.get('/webhook', async (req, res) => {
 router.post('/webhook', async (req, res) => {
     try {
         console.log(JSON.stringify(req.body));
+
+        const entry = req.body.entry[0];
+        const changes = entry.changes[0];
+        const contacts = changes[0].contacts;
+        const messages = changes[0].messages;
+
+        const username = contacts.profile.name;
+        const phone_number = contacts.profile.wa_id;
+
+        const message = messages[messages.length - 1];
+
+        console.log("From: ", username, " With number: ", phone_number)
+        console.log("Got Message: ", message)
     } catch(err) {
         console.log(err);
     }
 })
 
 export default router;
+
+/*  {
+    "object":"whatsapp_business_account",
+    "entry": [ { "id":"755604307393380",
+                 "changes": [
+                    { "value": { "messaging_product": "whatsapp",
+                          "contacts": [
+                            {
+                                "profile": {
+                                    "name":"Lova Reddy Dwarampudi"
+                                    },
+                                    "wa_id":"917337252906"
+                                    }
+                            ],
+                            "messages": [
+                              {
+                                   "from":"917337252906",
+                                   "id":"wamid.HBgMOTE3MzM3MjUyOTA2FQIAEhgUM0ZCQjdBOUU0N0Y0N0VFQkI5RTkA",
+                                   "timestamp":"1756877352",
+                                   "text":{"body":"Hi"},
+                                   "type":"text"
+                               }
+                             ]
+                      },
+                      "field":"messages"
+                      }]
+              }
+        ]
+} */
