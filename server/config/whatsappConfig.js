ded6454660d163fcef2e48f70a095d11811569b1
Nikhilcharan-dev <chintufru143@gmail.com>
2025-09-03 12:17:48 +0530

    Verifying

--- server/config/whatsappConfig.js
+++ server/config/whatsappConfig.js
@@ -20,12 +20,13 @@ router.post('/webhook', async (req, res) => {
         console.log(JSON.stringify(req.body));
 
         const entry = req.body.entry[0];
+        // entry[0].changes[0].value.contacts[0]
         const changes = entry.changes[0];
-        const contacts = changes[0].contacts[0];
-        const messages = changes[0].messages;
+        const contacts = changes[0].value.contacts[0];
+        const messages = changes[0].value.messages;
 
         const username = contacts.profile.name;
-        const phone_number = contacts.profile.wa_id;
+        const phone_number = contacts.wa_id;
 
         const message = messages[messages.length - 1];
 
@@ -38,31 +39,45 @@ router.post('/webhook', async (req, res) => {
 
 export default router;
 
-/*  {
-    "object":"whatsapp_business_account",
-    "entry": [ { "id":"755604307393380",
-                 "changes": [
-                    { "value": { "messaging_product": "whatsapp",
-                          "contacts": [
-                            {
-                                "profile": {
-                                    "name":"Lova Reddy Dwarampudi"
-                                    },
-                                    "wa_id":"917337252906"
-                                    }
-                            ],
-                            "messages": [
-                              {
-                                   "from":"917337252906",
-                                   "id":"wamid.HBgMOTE3MzM3MjUyOTA2FQIAEhgUM0ZCQjdBOUU0N0Y0N0VFQkI5RTkA",
-                                   "timestamp":"1756877352",
-                                   "text":{"body":"Hi"},
-                                   "type":"text"
-                               }
-                             ]
-                      },
-                      "field":"messages"
-                      }]
+/*
+{
+  "object": "whatsapp_business_account",
+  "entry": [
+    {
+      "id": "755604307393380",
+      "changes": [
+        {
+          "value": {
+            "messaging_product": "whatsapp",
+            "metadata": {
+              "display_phone_number": "15551468079",
+              "phone_number_id": "735313653000706"
+            },
+            "contacts": [
+              {
+                "profile": {
+                  "name": "Nikhil"
+                },
+                "wa_id": "917815873262"
               }
-        ]
-} */
+            ],
+            "messages": [
+              {
+                "from": "917815873262",
+                "id": "wamid.HBgMOTE3ODE1ODczMjYyFQIAEhgUM0Y4MzFCMkFDOUQ2MkFDQTE1OTAA",
+                "timestamp": "1756881698",
+                "text": {
+                  "body": "Heyyy"
+                },
+                "type": "text"
+              }
+            ]
+          },
+          "field": "messages"
+        }
+      ]
+    }
+  ]
+}
+
+ */
