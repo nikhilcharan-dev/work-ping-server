4f4008bef50d76ce8034197b7fd6a48af1f4f8c9
Nikhilcharan-dev <chintufru143@gmail.com>
2026-01-03 09:35:58 +0530

    CheckPoint 3

--- server/app/middleware.js
+++ server/app/middleware.js
@@ -23,13 +23,15 @@ export default function middlewares(app) {
     app.set("trust proxy", 1);
 
     app.use(cors(corsOptions));
-
     app.options(/.*/, cors(corsOptions));
 
     app.use(morgan("dev"));
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
     app.use(cookieParser());
+
     app.use((req, res, next) => {
         console.log("Origin IP:", req.ip);
         next();
