14c092a61cfc71b3088dbfba66f068ad7a077ac5
Nikhilcharan-dev <chintufru143@gmail.com>
2026-03-14 19:25:09 +0530

    Fixed Bug WorkType Not returning to Frontend

--- server/controllers/web/admin/team/member.controller.js
+++ server/controllers/web/admin/team/member.controller.js
@@ -136,7 +136,7 @@ export const getTeamMembers = asyncHandler(
                     from: "users",
                     localField: "userId",
                     foreignField: "_id",
-                    pipeline: [{ $project: { name: 1, email: 1, phone: 1, employeeId: 1, isActive: 1 } }],
+                    pipeline: [{ $project: { name: 1, email: 1, phone: 1, employeeId: 1, isActive: 1, workType: 1 } }],
                     as: "user"
                 }
             },
